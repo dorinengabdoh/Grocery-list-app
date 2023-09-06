b03500491a094701783fb9e5184f512f9e2551af
dorinengabdoh <ngabdohdorine@gmail.com>
2023-09-06 13:14:48 -0400

    fix lint error

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,61 +1,54 @@
 'use strict'
-let selectRow =null;
-let target ="";
+let selectedRow = ''
+const selectRow = null; //eslint-disable-line
+let target = ''
 // ---------------------delete data----------------------
-document.querySelector(".btn").addEventListener('click', (e) =>{
-  target = e.target;
-  if(target.classList.contains('delete')){
-    target.parentElement.parentElement.remove();
+document.querySelector('.btn').addEventListener('click', (e) => {
+  target = e.target
+  if (target.classList.contains('delete')) {
+    target.parentElement.parentElement.remove()
   }
 })
-
-function clear(){
-  document.querySelector(".ajout").value="";
-}
 // --------------------add-----------------
-document.querySelector('.add').addEventListener('click', (e) =>{
-  e.preventDefault();
+document.querySelector('.add').addEventListener('click', (e) => {
+  e.preventDefault()
   // get value
-  const ajouter = document.querySelector('.ajout').value;
-  //validate
-    // if(selectedRow == null) {
-      const list = document.querySelector(".btn");
-      const row = document.createElement("tr");
-      row.innerHTML = ` <td>${ajouter}</td>
+  const ajouter = document.querySelector('.ajout').value
+  // validate
+  // if(selectedRow == null) {
+  const list = document.querySelector('.btn')
+  const row = document.createElement('tr')
+  row.innerHTML = ` <td>${ajouter}</td>
                         <td>
                         <a href="#" class="edit">Edit</a>
                         <a href="#" class="delete">Delete</a>
-                      `;
-                        list.appendChild(row);
-                        selectRow = null;
+                      `
+  list.appendChild(row)
+  // selectRow = null
 })
 
-document.querySelector('.update').addEventListener('click', (e) =>{
-  e.preventDefault();
+document.querySelector('.update').addEventListener('click', (e) => {
+  e.preventDefault()
   // get value
-  const ajouter = document.querySelector('.ajout').value;
-  //validate
-    // if(selectedRow == null) {
-      const list = document.querySelector(".btn");
-      const row = document.createElement("tr");
-      row.innerHTML = ` <td>${ajouter}</td>
+  const ajouter = document.querySelector('.ajout').value
+  // validate
+  // if(selectedRow == null) {
+  const list = document.querySelector('.btn')
+  const row = document.createElement('tr')
+  row.innerHTML = ` <td>${ajouter}</td>
                         <td>
                         <a href="#" class="edit">Edit</a>
                         <a href="#" class="delete">Delete</a>
-                      `;
-                        list.appendChild(row);
-                        selectRow = null;  
+                      `
+  list.appendChild(row)
+  // selectRow = null
 })
 // // -----------------------edit-----------------------------------
 
 document.querySelector('.edit').addEventListener('click', (e) => {
-  target = e.target;
-  if (target.classList.contains('edit')){
-    selectedRow = target.parentElement.parentElement;
-    document.querySelector('.ajouter').value = selectedRow.children.textcontent;
+  target = e.target
+  if (target.classList.contains('edit')) {
+    selectedRow = target.parentElement.parentElement
+    document.querySelector('.ajouter').value = selectedRow.children.textcontent
   }
 })
-
-
-
-
